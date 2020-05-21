1e3e4cc27d38f18af4475a55da629e49900bcf4f
himanshu-prog <65400651+himanshu-prog@users.noreply.github.com>
2020-05-21 15:46:31 +0530

    Update Runner.java

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -2,7 +2,7 @@ package main;
 
 public class Runner {
 	public static void main(String ar[]) {
-		System .out.println("Maven project executed once again");
+		System .out.println("Maven project executed once again")
 	}
 
 }
