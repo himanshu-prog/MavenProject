decd9157316f0e4e4f2bbc68ddc09956b235f3a4
Ubuntu <himanshunexus5@gmail.com>
2020-05-21 06:25:36 +0000

    another commit

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -2,7 +2,7 @@ package main;
 
 public class Runner {
 	public static void main(String ar[]) {
-		System .out.println("Maven project executed");
+		System .out.println("Maven project executed once again");
 	}
 
 }
